55d5f245ec63b61c13d54c460aab70c2ba642bcf
Ajulll22 <ajulrizki@gmail.com>
2020-10-17 02:05:11 +0700

    PraktekWeb

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,4 +9,14 @@ class Home extends BaseController
 
 	//--------------------------------------------------------------------
 
+	public function show() {
+		$nama['title']='Ajulll';
+		echo view('mahasiswa/index', $nama);
+		echo view('mahasiswa/header', $nama);
+		echo view('mahasiswa/footer', $nama);
+		$nama['title']='1817051042';
+		echo view('mahasiswa/index', $nama);
+		echo view('mahasiswa/header', $nama);
+		echo view('mahasiswa/footer', $nama);
+	}
 }
